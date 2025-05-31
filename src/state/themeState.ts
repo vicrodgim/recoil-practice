3524c127cdda35668c74749e5a0de58f3e35f4c1
Victoria-Rodriguez <vicrodriguezgimenez@gmail.com>
2025-05-31 15:55:43 +0200

    added effects to themeState

--- src/state/themeState.ts
+++ src/state/themeState.ts
@@ -3,4 +3,15 @@ import { atom } from "recoil";
 export const themeState = atom<"light" | "dark">({
   key: "themeState",
   default: "light",
+  effects_UNSTABLE: [
+    ({ setSelf, onSet }) => {
+      const saved = localStorage.getItem("theme");
+      if (saved === "light" || saved === "dark") {
+        setSelf(saved);
+      }
+      onSet((newTheme) => {
+        localStorage.setItem("theme", newTheme);
+      });
+    },
+  ],
 });
